a4e8f7f8cb53cbc5a78357fcec3c1ff1c4dc10c0
sergiocruzjr <101006506+sergiocruzjr@users.noreply.github.com>
2022-04-09 23:20:01 -0300

    Regularizando git

---
+++ frontend/definicao-de-perfil1/cadastro.js
@@ -0,0 +1,86 @@
+var botaoCadastrar = document.querySelector(".btn-proximo");
+
+buscarUsuarios.addEventListener("click", function(){
+    const urlUsuarios = "";
+    fetch(urlUsuarios, {method:"POST", mode:"no-cors", params:usuario})
+
+
+
+/*botaoCadastrar.addEventListener("click", 
+function(event){
+    event.preventDefault();
+    var form = document.querySelector("#form-cadastro")
+
+    //Extraindo informações do usuario do form
+    var usuario = obtemUsuarioCadastrado(form)
+
+    //cria tr e td do usuario
+    var usuarioTr = montaTr(usuario)*/
+
+
+    form.reset();
+})
+
+function obtemUsuarioCadastrado(form){
+    
+    var usuario = {
+        nome: form.nome.value,
+        email: form.email.value,
+        senha: form.senha.value,
+        idade: form.idade.value,
+        cidade: form.cidade.value,
+        estado: form.estado.value,
+        cargo: form.cargo.value
+    }
+    return usuario;
+}
+
+
+/*var enviarUsuarios = document.querySelector("#finalizar-cadastro")
+
+enviarUsuarios.addEventListener("click", function(){
+    console.log(enviarUsuarios);
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("PUT", "localhost:3333/users")
+
+    /*xhr.addEventListener("load", function(){
+        var resposta = xhr.responseText;
+        var usuarios = JSON.parse(resposta);
+
+        usuarios.forEach(function(usuario){
+        
+
+        })
+    })
+
+    xhr.send();
+})*/
+
+
+function montaTr(usuario) {
+    var usuarioTr = document.createElement("tr");
+    usuarioTr.classList.add("usuario");
+
+    var nomeTd = document.createElement("td");
+    nomeTd.textContent = usuario.nome;
+    nomeTd.classList.add("info-nome");
+
+    var emailTd = document.createElement("td");
+    emailTd.textContent = usuario.email;
+    emailTd.classList.add("info-email");
+    
+    var senhaTd = document.createElement("td");
+    senhaTd.textContent = usuario.senha;
+    senhaTd.classList.add("info-senha");
+
+    usuarioTr.appendChild(nomeTd);
+    usuarioTr.appendChild(emailTd);
+    usuarioTr.appendChild(senhaTd);
+
+    var tabela = document.querySelector("#tabela-usuarios")
+    tabela.appendChild(usuarioTr);
+
+    return usuarioTr;
+}
