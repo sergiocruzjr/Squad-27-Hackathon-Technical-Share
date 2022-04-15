fd8107fe56e56e25fee90a9afe9437940ed6faef
sergiocruzjr <101006506+sergiocruzjr@users.noreply.github.com>
2022-04-15 19:21:32 -0300

    Últimas atualizações

--- frontend/Agendar2/agendar2.js
+++
@@ -1,6 +0,0 @@
-let startDate = document.getElementById('startDate')
-
-startDate.addEventListener('change',(e)=>{
-  let startDateVal = e.target.value
-  document.getElementById('startDateSelected').innerText = startDateVal
-})
